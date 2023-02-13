3b30d6c2abf820f30ae2f3f601a4c5a3c5edde44
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2023-02-13 13:45:58 +0300

    start

---
+++ awsome/main.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three'
+
+//scene
+const scene = new THREE.Scene();
+
+// create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+  color : '#00ff83',
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+// camera
+const camera = new THREE.PerspectiveCamera(45, 800/600)
+camera.position.z = 10
+scene.add(camera)
+
+
+//renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(800,600)
+renderer.render(scene, camera)
